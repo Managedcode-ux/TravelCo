e0a2a8315339cb71b862c6d6ec47b0e78f044725
Managedcode-ux <71963227+Managedcode-ux@users.noreply.github.com>
2025-07-15 13:54:45 +0530

    Modified the css so that root container spans complete height of the viewport width when working in mobile dimenstion also preventing the overflow issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { Button, Title, Text, Container } from '@mantine/core';
-
-import {Routes, Route, Link} from "react-router-dom"
+import "./App.css"
+import { Routes, Route, Link } from "react-router-dom"
 import ToursPage from './pages/allTours/allTours';
 import TourDetails from './pages/tourDetails/tourdetails';
 
 export default function App() {
   return (
-    <Routes>
-      <Route path='/allTours' element={<ToursPage/>}/>
-      <Route path='/tour/:id' element={<TourDetails/>}/>
-    </Routes>
+    <div className='rootContainer'>
+      <Routes>
+        <Route path='/allTours' element={<ToursPage />} />
+        <Route path='/tour/:id' element={<TourDetails />} />
+      </Routes>
+    </div>
   );
 }
 
